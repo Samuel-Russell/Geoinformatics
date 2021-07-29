567e5bb3beadcf8fb5e176079546d5a778236c23
Samuel Russell <69426323+Samuel-Russell@users.noreply.github.com>
2021-07-29 14:05:20 +0200

    Add files via upload

---
+++ Timeseries_CSV_Read_Plot.py
@@ -0,0 +1,19 @@
+## Basic Time-Series Plot Using Pandas
+
+# Reading Packages
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Reading Data
+df = pd.read_csv('weather_data_GER_2016.csv') 
+
+# Creating Variables
+date = pd.to_datetime(df['timestamp'])
+precip = df['p']
+
+# Plotting Data
+fig, ax = plt.subplots()
+ax.plot(date, precip)
+plt.title('Air Pressure, Germany, MERRA-2 weather data, 2016')
+plt.xlabel('Date')
+plt.ylabel('Air Pressure [Pa]')
