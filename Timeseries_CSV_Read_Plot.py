63edc2638795e2bba443f7c9c545a903ca859895
Samuel Russell <69426323+Samuel-Russell@users.noreply.github.com>
2021-07-30 10:37:24 +0200

    Update Timeseries_CSV_Read_Plot.py

--- Timeseries_CSV_Read_Plot.py
+++ Timeseries_CSV_Read_Plot.py
@@ -10,11 +10,11 @@ df = pd.read_csv('weather_data_GER_2016.csv')
 
 # Creating Variables
 date = pd.to_datetime(df['timestamp'])
-precip = df['p']
+pressure = df['p']
 
 # Plotting Data
 fig, ax = plt.subplots()
-ax.plot(date, precip)
+ax.plot(date, pressure)
 plt.title('Air Pressure, Germany, MERRA-2 weather data, 2016')
 plt.xlabel('Date')
 plt.ylabel('Air Pressure [Pa]')
